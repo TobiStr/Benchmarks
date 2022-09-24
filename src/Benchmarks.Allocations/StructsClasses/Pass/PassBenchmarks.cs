730456e737a66992b50f3724394c46bc0973829a
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-09-24 11:31:25 +0200

    :+1: Add PassBenchmarks

---
+++ src/Benchmarks.Allocations/StructsClasses/Pass/PassBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿namespace Benchmarks.Allocations.StructsClasses.Pass;
+
+[MemoryDiagnoser]
+public class PassBenchmarks
+{
+    [Params(10, 100, 1000)]
+    public int PassCount { get; set; }
+
+    [Benchmark]
+    public void PassClasses()
+    {
+        var x = new TestClass(1);
+        _ = PassClassesInternal(x, 0);
+    }
+
+    [Benchmark]
+    public void PassStructs()
+    {
+        var x = new TestStruct(1);
+        _ = PassStructsInternal(x, 0);
+    }
+
+    private int PassClassesInternal(TestClass testClass, int count)
+    {
+        if (count >= PassCount) return count;
+        return PassClassesInternal(testClass, count + 1);
+    }
+
+    private int PassStructsInternal(TestStruct testStruct, int count)
+    {
+        if (count >= PassCount) return count;
+        return PassStructsInternal(testStruct, count + 1);
+    }
+}
