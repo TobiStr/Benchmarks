98f78ff96f60cb6c5e8079673c7888824fa6c29f
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-09-24 18:31:09 +0200

    Create ClosuresBenchmarks.cs

---
+++ src/Benchmarks.Allocations/Closures/ClosuresBenchmarks.cs
@@ -0,0 +1,28 @@
+﻿namespace Benchmarks.Allocations.Closures;
+
+[MemoryDiagnoser]
+public class ClosuresBenchmarks
+{
+    [Benchmark]
+    public void Closure()
+    {
+        int x = 1;
+
+        _ = Enumerable.Range(0, 1000000)
+            .Select(i => i + x)
+            .ToArray();
+    }
+
+    [Benchmark]
+    public void NonClosure()
+    {
+        const int length = 1000000;
+
+        var results = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            results[i] = i + 1;
+        }
+    }
+}
