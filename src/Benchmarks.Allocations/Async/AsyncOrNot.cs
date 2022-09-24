f6feb6d5708fd919b05547236bc9cfc865891fb7
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-09-24 18:31:15 +0200

    Create AsyncOrNot.cs

---
+++ src/Benchmarks.Allocations/Async/AsyncOrNot.cs
@@ -0,0 +1,19 @@
+﻿namespace Benchmarks.Allocations.Async;
+
+[MemoryDiagnoser]
+public class AsyncOrNot
+{
+    [Benchmark]
+    public void GetNumbers()
+    {
+        for (int i = 0; i < 1000; i++)
+            _ = Enumerable.Range(0, 10).ToArray();
+    }
+
+    [Benchmark]
+    public async Task GetNumbersAsync()
+    {
+        for (int i = 0; i < 1000; i++)
+            _ = await Task.Run(() => Enumerable.Range(0, 10).ToArray());
+    }
+}
