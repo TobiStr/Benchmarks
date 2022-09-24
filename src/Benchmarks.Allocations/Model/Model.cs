c47bb84464961fe8579d3d4e130c28bac8af9b01
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-09-24 11:30:04 +0200

    :construction: Add Test Models

---
+++ src/Benchmarks.Allocations/Model/Model.cs
@@ -0,0 +1,23 @@
+﻿namespace Benchmarks.Allocations.Model;
+
+public struct TestStruct
+{
+    public int Value { get; set; }
+
+    public TestStruct(int value)
+    {
+        Value = value;
+    }
+}
+
+public class TestClass
+{
+    public int Value { get; set; }
+
+    public TestClass(int value)
+    {
+        Value = value;
+    }
+}
+
+public readonly record struct TestRecordStruct(int Value);
