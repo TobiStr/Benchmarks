bbf9239546d0b6983b471e976790b15146c99122
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-09-24 11:31:05 +0200

    :+1: Add InitializeBenchmarks

---
+++ src/Benchmarks.Allocations/StructsClasses/Initialize/InitializeBenchmarks.cs
@@ -0,0 +1,23 @@
+﻿namespace Benchmarks.Allocations.StructsClasses.Initialize;
+
+[MemoryDiagnoser]
+public class InitializeBenchmarks
+{
+    [Benchmark]
+    public void InitializeStruct()
+    {
+        _ = new TestStruct(1);
+    }
+
+    [Benchmark]
+    public void InitializeClass()
+    {
+        _ = new TestClass(1);
+    }
+
+    [Benchmark]
+    public void InitializeRecordStruct()
+    {
+        _ = new TestRecordStruct(1);
+    }
+}
