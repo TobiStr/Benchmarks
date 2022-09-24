e6b772ff2445343a7bb39d2d04c0c6fbb28981d0
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-09-24 18:30:56 +0200

    Create LoggingBenchmarks.cs

---
+++ src/Benchmarks.Allocations/Logging/LoggingBenchmarks.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Benchmarks.Allocations.Logging;
+
+[MemoryDiagnoser]
+public class LoggingBenchmarks
+{
+    private ILogger logger;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var loggerFactory = LoggerFactory.Create(builder =>
+            builder.AddConsole().SetMinimumLevel(LogLevel.Warning)
+        );
+        logger = loggerFactory.CreateLogger<LoggingBenchmarks>();
+    }
+
+    [Benchmark]
+    public void LogString()
+    {
+        for (int i = 0; i < 1000; i++)
+            logger.LogInformation(Random.Shared.Next().ToString());
+    }
+
+    [Benchmark]
+    public void LogStringInterpolation()
+    {
+        for (int i = 0; i < 1000; i++)
+            logger.LogInformation($"A{Random.Shared.Next()}");
+    }
+
+    [Benchmark]
+    public void LogStringTemplate()
+    {
+        const string template = "A{Test}";
+
+        for (int i = 0; i < 1000; i++)
+            logger.LogInformation(template, Random.Shared.Next());
+    }
+}
