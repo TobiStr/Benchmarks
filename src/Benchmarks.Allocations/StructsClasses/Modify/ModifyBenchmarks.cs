5a0d0eef3aa449014a21600e4ce40ec6ece4e8e1
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-09-24 11:31:15 +0200

    :+1: Add ModifyBenchmarks

---
+++ src/Benchmarks.Allocations/StructsClasses/Modify/ModifyBenchmarks.cs
@@ -0,0 +1,40 @@
+﻿namespace Benchmarks.Allocations.StructsClasses.Modify;
+
+public class ModifyBenchmarks
+{
+    [Params(10, 100, 1000)]
+    public int ModifyCount { get; set; }
+
+    [Benchmark]
+    public void ModifyClass()
+    {
+        var x = new TestClass(1);
+
+        for (int i = 0; i < ModifyCount; i++)
+        {
+            x.Value = i;
+        }
+    }
+
+    [Benchmark]
+    public void ModifyStruct()
+    {
+        var x = new TestStruct(1);
+
+        for (int i = 0; i < ModifyCount; i++)
+        {
+            x.Value = i;
+        }
+    }
+
+    [Benchmark]
+    public void ModifyRecordStruct()
+    {
+        var x = new TestRecordStruct(1);
+
+        for (int i = 0; i < ModifyCount; i++)
+        {
+            _ = x with { Value = i };
+        }
+    }
+}
