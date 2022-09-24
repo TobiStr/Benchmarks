ac9bbd35a72f7cd38e6fbfaaa463c5e572fb6055
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-09-24 18:30:58 +0200

    Create ThrowBenchmarks.cs

---
+++ src/Benchmarks.Allocations/Exceptions/ThrowBenchmarks.cs
@@ -0,0 +1,67 @@
+﻿namespace Benchmarks.Allocations.Exceptions;
+
+[MemoryDiagnoser]
+public class ThrowBenchmarks
+{
+    [Benchmark]
+    public void NoTryCatch()
+    {
+        Operation();
+    }
+
+    [Benchmark]
+    public void TryCatchWithoutThrow()
+    {
+        TryCatchWithoutThrowInternal();
+    }
+
+    [Benchmark]
+    public void NoTryCatch1000()
+    {
+        for (int i = 0; i < 1000; i++)
+            Operation();
+    }
+
+    [Benchmark]
+    public void TryCatchWithoutThrow1000()
+    {
+        for (int i = 0; i < 1000; i++)
+            TryCatchWithoutThrowInternal();
+    }
+
+    [Benchmark]
+    public void TryCatchWithThrow()
+    {
+        TryCatchWithThrowInternal();
+    }
+
+    private void TryCatchWithoutThrowInternal()
+    {
+        try
+        {
+            Operation();
+        }
+        catch (Exception ex)
+        {
+            //Ignore
+        }
+    }
+
+    private void TryCatchWithThrowInternal()
+    {
+        try
+        {
+            Operation();
+            throw new InvalidOperationException();
+        }
+        catch (Exception ex)
+        {
+            //Ignore
+        }
+    }
+
+    private void Operation()
+    {
+        _ = Enumerable.Range(0, 100).ToArray();
+    }
+}
