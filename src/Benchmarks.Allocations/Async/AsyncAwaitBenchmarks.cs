d6a2eb006a652a3d6d60c7036bae8a4073f38bda
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-09-24 18:31:30 +0200

    :+1: Add AsyncAwaitBenchmarks

---
+++ src/Benchmarks.Allocations/Async/AsyncAwaitBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿namespace Benchmarks.Allocations.Async;
+
+[MemoryDiagnoser]
+public class AsyncAwaitBenchmarks
+{
+    [Benchmark]
+    public async Task WithAwait()
+    {
+        await WithAwaitInternal();
+    }
+
+    [Benchmark]
+    public async Task WithOutAwait()
+    {
+        await WithOutAwaitInternal();
+    }
+
+    private async Task WithAwaitInternal()
+    {
+        await InternalTask();
+    }
+
+    private Task WithOutAwaitInternal()
+    {
+        return InternalTask();
+    }
+
+    private async Task InternalTask()
+    {
+        await Task.Delay(10);
+    }
+}
